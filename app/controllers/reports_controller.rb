2138ece62380dc07ef5e488a88e67e6c0536a52a
Enrique <egonzalez78@gmail.com>
2014-12-26 12:33:46 -0800

    updated the focus question/prompt

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,13 +1,17 @@
 class ReportsController < ApplicationController
+
   def index
-    @entries = Entry.last_five_days(current_user)
 
-    # Mood Shift
-    avg_start_feeling = @entries.average(:how_do_you_feel)
-    avg_end_feeling = @entries.average(:how_do_you_feel_now)
+    if user_signed_in?
+      @entries = Entry.last_five_days(current_user)
 
-    @mood_start = mood(avg_start_feeling)
-    @mood_end = mood(avg_end_feeling)
+      # Mood Shift
+      avg_start_feeling = @entries.average(:how_do_you_feel)
+      avg_end_feeling = @entries.average(:how_do_you_feel_now)
+      @mood_start = mood(avg_start_feeling)
+      @mood_end = mood(avg_end_feeling)
+    end
+
   end
 
 end
