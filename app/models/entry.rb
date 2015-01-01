99e37c9c6f7ff7e94088945545c2bac87e9a5dbe
Enrique <egonzalez78@gmail.com>
2015-01-01 13:05:25 -0800

    added specific paths to home partials, in case I want to render the root entry form on error

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,7 +3,10 @@ class Entry < ActiveRecord::Base
 
   scope :last_five_days, -> (user) { where("date >= ? AND user_id = ?", 5.days.ago, user.id).limit(5) }
 
+  validates :date, uniqueness: true
+  validates_presence_of :how_do_you_feel, :how_do_you_feel_now
   before_create :set_todays_date
+
 
   def set_todays_date
     self.date = Date.today
