ad422a00b49be1ba2d4eeb9863e51aef401d80fb
Enrique <egonzalez78@gmail.com>
2015-01-03 23:57:25 -0800

    updated the mood shift view helper

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,10 +1,10 @@
 module ReportsHelper
   def mood_shift(feeling_start,feeling_end)
-    if feeling_start < feeling_end
+    if feeling_start < feeling_end && mood(feeling_start) != mood(feeling_end)
       "Oh yeah! Your mood is getting better"
-    elsif feeling_start == feeling_end && mood(feeling_end) == "happy"
+    elsif mood(feeling_start) == mood(feeling_end) && mood(feeling_end) == "happy"
       "You're a happy camper"
-    elsif feeling_start == feeling_end
+    elsif mood(feeling_start) == mood(feeling_end)
       "Your mood is staying samesies"
     else
       "Uh oh! Your mood is worse"
