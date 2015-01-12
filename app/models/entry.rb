a61940e0efb96f9ff9bfe82fc5677510089760f8
Enrique <egonzalez78@gmail.com>
2015-01-12 14:35:03 -0800

    fixed scope issue on entries

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,8 +2,8 @@ class Entry < ActiveRecord::Base
   belongs_to :user
 
   scope :last_five_days, -> (user) { where("date >= ? AND user_id = ?", 5.days.ago, user.id).limit(5) }
-  scope :yesterday, -> (user) { where("date >= ? AND user_id = ?", Date.yesterday, user.id) }
-  scope :today, -> (user) { where("date >= ? AND user_id = ?", Date.today, user.id) }
+  scope :yesterday, -> (user) { where("date = ? AND user_id = ?", Date.yesterday, user.id) }
+  scope :today, -> (user) { where("date = ? AND user_id = ?", Date.today, user.id) }
 
   validates_uniqueness_of :date, :scope => :user_id
   validates_presence_of :how_do_you_feel, :how_do_you_feel_now
