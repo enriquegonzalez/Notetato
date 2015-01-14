cec35948ce62993be456bb94e0c2406dbf7b9e2b
Enrique <egonzalez78@gmail.com>
2015-01-13 16:14:16 -0800

    updating the sample report ad

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
     before_action :set_entry, only: [:show, :edit, :update, :destroy]
   def index
-    if Entry.today(current_user).exists?
+    if user_signed_in? && Entry.today(current_user).exists?
       respond_to do |format|
         format.html { redirect_to reports_path }
       end
