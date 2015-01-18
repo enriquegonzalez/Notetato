ea96ec88c6ab03289e1dac1859c4d205b7baeabc
Enrique <egonzalez78@gmail.com>
2015-01-18 11:33:58 -0800

    configured ENV variables, and tested mailer

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+app_env_vars = File.join(Rails.root, 'config', 'initializers', '01_development_env.rb')
+load(app_env_vars) if File.exists?(app_env_vars)
+
 # Initialize the Rails application.
 Rails.application.initialize!
