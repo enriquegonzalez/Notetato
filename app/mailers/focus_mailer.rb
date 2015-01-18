daa18a75705beaae20aba3720cae18e347129125
Enrique <egonzalez78@gmail.com>
2015-01-17 21:14:10 -0800

    removed focusmailer and generated reportmailer

--- app/mailers/focus_mailer.rb
+++
@@ -1,3 +0,0 @@
-class FocusMailer < ActionMailer::Base
-  default from: "from@example.com"
-end
--- test/mailers/previews/focus_mailer_preview.rb
+++
@@ -1,4 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/focus_mailer
-class FocusMailerPreview < ActionMailer::Preview
-
-end
