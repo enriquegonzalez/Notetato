2c47665421a0c65c89db21c1bc369e9abc056295
Enrique <egonzalez78@gmail.com>
2015-01-21 17:40:47 -0800

    configured mandrill for smtp in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,8 +89,8 @@ Rails.application.configure do
     port:                 587,
     domain:               'heroku.com',
     user_name:            ENV['MANDRILL_USERNAME'],
-    password:             ENV['MANDRILL_APIKEY'] #,
-    # authentication:       'plain',
-    # enable_starttls_auto: true  }
+    password:             ENV['MANDRILL_APIKEY'],
+    authentication:       'plain',
+    enable_starttls_auto: true
   }
 end
