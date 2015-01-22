7970af278e118498199703036a923dce93dd4764
Enrique <egonzalez78@gmail.com>
2015-01-21 17:20:12 -0800

    configured mandrill for smtp in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
 
     config.action_mailer.default_url_options = { host: 'https://notetato.herokuapp.com' }
 
-  # config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
   #   address:              'smtp.mandrillapp.com',
   #   port:                 587,
