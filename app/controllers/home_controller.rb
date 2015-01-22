b88da5483847f5840e65ff1d280761128a120fe3
Enrique <egonzalez78@gmail.com>
2015-01-21 21:33:56 -0800

    updated the redirect logic in root for logged in users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,10 @@ class HomeController < ApplicationController
       respond_to do |format|
         format.html { redirect_to reports_path }
       end
+    elsif user_signed_in? && !Entry.today(current_user).exists?
+      respond_to do |format|
+        format.html { redirect_to new_entry_path }
+      end
     else
       @entry = Entry.new
     end
