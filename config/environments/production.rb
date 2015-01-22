7e666db7eeefd29df1565983602ca45663088268
Enrique <egonzalez78@gmail.com>
2015-01-21 17:25:17 -0800

    configured mandrill for smtp in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,8 +85,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.mandrillapp.com'#,
-    # port:                 587,
+    address:              'smtp.mandrillapp.com',
+    port:                 587 #,
     # domain:               'heroku.com',
     # user_name:            ENV['MANDRILL_USERNAME'],
     # password:             ENV['MANDRILL_APIKEY'],
