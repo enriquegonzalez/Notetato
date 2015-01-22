898482b19f313b0ea13b5d3c4adb5f851897b248
Enrique <egonzalez78@gmail.com>
2015-01-21 17:22:10 -0800

    configured mandrill for smtp in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,13 +84,13 @@ Rails.application.configure do
     config.action_mailer.default_url_options = { host: 'https://notetato.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   address:              'smtp.mandrillapp.com',
-  #   port:                 587,
-  #   domain:               'heroku.com',
-  #   user_name:            ENV['MANDRILL_USERNAME'],
-  #   password:             ENV['MANDRILL_APIKEY'],
-  #   authentication:       'plain',
-  #   enable_starttls_auto: true  }
-  # }
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.mandrillapp.com'#,
+    # port:                 587,
+    # domain:               'heroku.com',
+    # user_name:            ENV['MANDRILL_USERNAME'],
+    # password:             ENV['MANDRILL_APIKEY'],
+    # authentication:       'plain',
+    # enable_starttls_auto: true  }
+  }
 end
