eb143c903652bd74b46c5b9dd4af51e822527c0b
Enrique <egonzalez78@gmail.com>
2015-01-23 11:12:56 -0800

    added a random default message-id to mailer and updated some daily email copy

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -1,11 +1,13 @@
 class ReportMailer < ActionMailer::Base
   default from: "notetato.app@gmail.com"
+  # default "Message-ID"=>"#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@gmail.com"
+  default "Message-ID" => lambda {"<#{SecureRandom.uuid}@gmail.com"}
 
   def focus_email(recipient, focus)
     @recipient = recipient
     @url  = 'http://notetato.com'
     @focus = focus
-    mail(to: @recipient.email, subject: "Today's focus")
+    mail(to: @recipient.email, subject: "Your Daily Focus Report")
   end
 
 end
