84ffe7bb3ff8fb032a2581f841ff5319ea1751ce
Enrique <egonzalez78@gmail.com>
2015-01-23 14:08:24 -0800

    fixed current_user typo, missing symbol :current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   helper_method :mood
 
   # before_filter :authenticate_user!
-  around_filter :user_time_zone, if: @current_user
+  around_filter :user_time_zone, if: :current_user
 
   def after_sign_in_path_for(resource_or_scope)
     if cookies[:guest_entry] == "exists"
