c7d9e68104cea9a080e7a328633e3a2ac2315082
Enrique <egonzalez78@gmail.com>
2015-01-24 17:48:35 -0800

    filtered parameters

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += [:password, :email, :how_do_you_feel, :what_went_well, :focus_on_tomorrow, :how_do_you_feel_now, :user_id]
