2239101080ad753b4d2f6ef985ffcd764ddb99f9
Enrique <egonzalez78@gmail.com>
2015-01-25 20:22:59 -0800

    updated this week scope with date current in users timezone

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,7 +1,7 @@
 class Entry < ActiveRecord::Base
   belongs_to :user
 
-  scope :this_week, -> (user) { where("date >= ? AND user_id = ?", Date.current.beginning_of_week, user.id) }
+  scope :this_week, -> (user) { where("date >= ? AND user_id = ?", Date.current.in_time_zone(user.time_zone).beginning_of_week.to_date, user.id) }
   scope :yesterday, -> (user) { where("date = ? AND user_id = ?", Date.yesterday.in_time_zone(user.time_zone), user.id) }
   scope :today, -> (user) { where("date = ? AND user_id = ?", Date.current.in_time_zone(user.time_zone), user.id) }
 
